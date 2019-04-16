cc21be9f5b4a6d3d82558006db2da756402fc31f
HyunWoo Jung <hyunsama@snu.ac.kr>
2019-04-16 23:11:02 +0900

    basic configuration, admin/1234

--- gwajams/gwajams/settings.py
+++ gwajams/gwajams/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1^ao)q0z*1q-$^8kbn-#9(gj)pf7w$3uktt+kg!7sg26d-4riq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '127.0.0.1', 'localhost' ]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main.apps.MainConfig',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
