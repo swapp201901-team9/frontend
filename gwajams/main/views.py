19c2a8c182da30034f175d7bf32aa9b81ed77d46
HyunWoo Jung <hyunsama@snu.ac.kr>
2019-04-17 02:50:52 +0900

    template test

--- gwajams/main/views.py
+++ gwajams/main/views.py
@@ -4,6 +4,10 @@ from django.views.generic.base import TemplateView
 class index(TemplateView):
     template_name = 'main/index.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['session'] = {'username':'admin'}
+
 class login(TemplateView):
     template_name = 'main/index.html'
 
