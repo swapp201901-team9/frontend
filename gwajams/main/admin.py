eb2c386527eafbdec5820741c5c3c4f1096fee53
HyunWoo Jung <hyunsama@snu.ac.kr>
2019-04-17 00:54:47 +0900

    model create

--- gwajams/main/admin.py
+++ gwajams/main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from main.models import Design, User, Group
 
-# Register your models here.
+admin.site.register(Group)
+admin.site.register(User)
+admin.site.register(Design)
