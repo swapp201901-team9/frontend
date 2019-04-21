889327967691c5f57eb3f3112bd484127fd5b240
sally20921 <sally20921@snu.ac.kr>
2019-04-21 14:24:12 +0900

    changed UI

--- progress/Test_back_local.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-python3 backend_article_tests.py http://127.0.0.1:8000/
-python3 backend_chatting_tests.py http://127.0.0.1:8000/
-python3 backend_friend_tests.py http://127.0.0.1:8000/
-python3 backend_wall_tests.py http://127.0.0.1:8000/
-python3 backend_profile_tests.py http://127.0.0.1:8000/
-python3 backend_sasang_tests.py http://127.0.0.1:8000/
-#python3 backend_image_tests.py http://127.0.0.1:8000/
--- progress/runserver.sh
+++
@@ -1,1 +0,0 @@
-tmux new-session -s new-session -n backend "python3 ../northfacebook_backend/manage.py runserver 0.0.0.0:8000" \; new-window -n frontend "cd ../northfacebook_frontend; npm start" \; new-window -n testWindow "echo 'tmux kill-session -t new-session'"
