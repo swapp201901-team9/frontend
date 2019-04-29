66397d6cca0369df084aca54f8ad10466601d11a
Seunga Shin <tmddk2709@naver.com>
2019-04-29 14:52:15 +0900

    edit CreateGroup.js

--- frontend/src/components/GroupPage/CreateGroup.js
+++ frontend/src/components/GroupPage/CreateGroup.js
@@ -2,28 +2,55 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 class CreateGroupPage extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			grouptype: 'Select an Option',
+			groupname: ''
+		};
+	}
+
+	const type_options = [
+		'Select an Option',
+		'Club',
+		'Department',
+		'Else'
+	]
+
+	const onChange(e) {
+		this.setState({
+			grouptype: e.target.grouptype,
+			groupname: e.target.groupname
+		})
+	}
+	
+	const onSubmit = () => {
+		//SA TODO 기존에 있는 이름과 같은 이름일 경우 경고
+		this.props.onCreateGroup(grouptype, groupname)
+	}
+	
 	render() {
 		return(
 			<div className="CreateGroup">
-				<form>
-					<input
-						type="radio"
-						name="grouptype"
-						placeholder={this.props.grouptype}
-						value="club"
-					/> 동아리
-					<input
-						type="radio"
-						name="grouptype"
-						placeholder={this.props.grouptype}
-						value="department"
-					/> 학과
-				</form>
+				<label htmlFor="group type">Group Type</label>
+				<select 
+					value={this.state.grouptype}
+					onChange={this.onChange.bind(this)}
+					className="type-select"
+				/> 
+					{options.map(option => {
+						return <option value={option} key={option} >{option</option>
+					})}
+				</select>
+				<label htmlFor="group name">Group Name</label>
 				<input 
 					type="text"
-					name="groupname"
-					placeholder={this.props.groupname}
+					value={this.state.groupname}
+					onChange={this.onChange.bind(this})
+					className="name-input"
 				/>
+				<button onClick={this.onSubmit.bind(this)}>CREATE GROUP</button>
 			</div>
 
 		)
@@ -36,9 +63,9 @@ const mapStateToProps = (state) => ({
 })
 
 const mapDispatchToProps = (dispatch) => ({
-
+	onCreateGroup: (grouptype, groupname) => dispatch(greateGroup(grouptype, groupname))
 })
 
-export default connect( mapStateToProps, mapDispatchToProps )(CreateGroupPage)
+export default connect( null, mapDispatchToProps )(CreateGroupPage)
 
 
