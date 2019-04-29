60cda766f4ac0075d07caff9e9bb1349be95c3c8
Seunga Shin <tmddk2709@naver.com>
2019-04-29 16:49:48 +0900

    fix bugs

--- frontend/src/components/GroupPage/CreateGroup.js
+++ frontend/src/components/GroupPage/CreateGroup.js
@@ -1,33 +1,35 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-class CreateGroupPage extends React.Component {
+import { createGroup } from '../../actions/index.js'
+
+class CreateGroup extends React.Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
 			grouptype: 'Select an Option',
 			groupname: ''
-		};
+		}
 	}
 
-	const type_options = [
+	type_options = [
 		'Select an Option',
 		'Club',
 		'Department',
 		'Else'
-	]
+		]
 
-	const onChange(e) {
+	onChange = (e) => {
 		this.setState({
 			grouptype: e.target.grouptype,
 			groupname: e.target.groupname
 		})
 	}
 	
-	const onSubmit = () => {
+	onSubmit = () => {
 		//SA TODO 기존에 있는 이름과 같은 이름일 경우 경고
-		this.props.onCreateGroup(grouptype, groupname)
+		this.props.onCreateGroup(this.state.grouptype, this.state.groupname)
 	}
 	
 	render() {
@@ -38,16 +40,16 @@ class CreateGroupPage extends React.Component {
 					value={this.state.grouptype}
 					onChange={this.onChange.bind(this)}
 					className="type-select"
-				/> 
-					{options.map(option => {
-						return <option value={option} key={option} >{option</option>
+				> 
+					{this.type_options.map(option => {
+						return <option value={option} key={option} >{option}</option>
 					})}
 				</select>
 				<label htmlFor="group name">Group Name</label>
 				<input 
 					type="text"
 					value={this.state.groupname}
-					onChange={this.onChange.bind(this})
+					onChange={this.onChange.bind(this)}
 					className="name-input"
 				/>
 				<button onClick={this.onSubmit.bind(this)}>CREATE GROUP</button>
@@ -58,9 +60,9 @@ class CreateGroupPage extends React.Component {
 }
 
 const mapDispatchToProps = (dispatch) => ({
-	onCreateGroup: (grouptype, groupname) => dispatch(greateGroup(grouptype, groupname))
+	onCreateGroup: (grouptype, groupname) => dispatch(createGroup(grouptype, groupname))
 })
 
-export default connect( null, mapDispatchToProps )(CreateGroupPage)
+export default connect( null, mapDispatchToProps )(CreateGroup)
 
 
