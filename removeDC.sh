881c28bbceda3204dc6d2b9cc5c30d63ba7b092b
sally20921 <sally20921@snu.ac.kr>
2019-04-30 15:34:17 +0900

    last commit

--- removeDC.sh
+++ removeDC.sh
@@ -1,7 +1,7 @@
-rm -f northfacebook_backend/tmp.db db.sqlite3
-rm -r northfacebook_backend/homepage/migrations
-rm -r northfacebook_backend/media/articles
-rm -r northfacebook_backend/media/profiles
+rm -f backend/tmp.db db.sqlite3
+rm -r backend/homepage/migrations
+rm -r backend/media/articles
+rm -r backend/media/default
 rm -f northfacebook_backend/homepage/__pycache__/*
 rm -f northfacebook_backend/northfacebook_backend/__pycache__/*
 rm -f progress/__pycache__/*
