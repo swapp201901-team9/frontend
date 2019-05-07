5c5368792640ac1ae66246a739d15ef2b680cd2e
sally20921 <sally20921@snu.ac.kr>
2019-05-07 15:42:22 +0900

    separate frontend backend repo

--- backend/homepage/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from homepage.models import Design, Profile, Group
-
-admin.site.register(Group)
-admin.site.register(Profile)
-admin.site.register(Design)
--- backend/removeDB.sh
+++
@@ -1,4 +0,0 @@
-rm -f tmp.db db.sqlite3
-rm -r homepage/migrations
-python3 manage.py makemigrations homepage
-python3 manage.py migrate
