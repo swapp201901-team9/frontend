378dcf7251e613cfc98db89d49db113b772a3f83
SeungaShin <tmddk2709@naver.com>
2019-05-12 10:22:04 +0900

    Fixed the loading problem(removed wrong state in mapStateToProps)

--- frontend/src/components/ProfilePage/ProfilePage.js
+++ frontend/src/components/ProfilePage/ProfilePage.js
@@ -5,38 +5,40 @@ import ChangePWPage from './ChangePWPage';
 import EscapePage from './EscapePage';
 
 class ProfilePage extends React.Component {
-
     render() {
-        return (
-              <div>
-              <NavBar />
-                  <section className="wrap clear">
+      if(!this.props.loading) {
+        return(
+          <p>loading..</p>
+        )
+      }
+      return (
+            <div>
+            <NavBar />
+                <section className="wrap clear">
 
-                      <div className="main">
-                      <h2 className="h_white">MY PAGE</h2>
-                        <div className="Text-Field">
-                          <label>
-                          ID
-                          </label>
-                          <div className="bordered_textarea">{this.props.profile_user}</div>
+                    <div className="main">
+                    <h2 className="h_white">MY PAGE</h2>
+                      <div className="Text-Field">
+                        <label>
+                        ID
+                        </label>
+                        <div className="bordered_textarea">{this.props.profile_user}</div>
+                      </div>
+                    
+                        <ChangePWPage/>
+                        <EscapePage/>
                         </div>
-                      
-                          <ChangePWPage/>
-                          <EscapePage/>
-                          </div>
 
-                  </section>
-              </div>
-             )
+                </section>
+            </div>
+            )
     }
 }
 
-let mapStateToProps = (state) => {
+const mapStateToProps = (state) => {
     return {
-
-        profile_user : Object.assign(state.authorization).split(":")[0],
-        profile_myname: state.profile_user !== null? Object.assign(state.profile_user.myname): null,
-        profile_mybelong: state.profile_user !== null? Object.assign(state.profile_user.mybelong):null,
+        loading: state.loading,
+        profile_user : Object.assign(state.authorization).split(":")[0]
 
     }
 }
