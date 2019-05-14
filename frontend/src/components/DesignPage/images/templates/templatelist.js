08d3afe9439975e74d8a0e72ffd44ff3c3d7ae4b
sally20921 <sally20921@snu.ac.kr>
2019-05-14 16:22:17 +0900

    done

--- frontend/src/components/DesignPage/images/templates/templatelist.js
+++ frontend/src/components/DesignPage/images/templates/templatelist.js
@@ -80,34 +80,140 @@ const faciallist = [
 
 const back_arm = [
 	require('./back_arm/2a2a2a.png'),
+	require('./back_arm/2e4373.png'),
+	require('./back_arm/4a321f.png'),
+	require('./back_arm/4f4f53.png'),
+	require('./back_arm/485a2f.png'),
+	require('./back_arm/316863.png'),
+	require('./back_arm/462355.png'),
+	require('./back_arm/942727.png'),
+	require('./back_arm/a65824.png'),
+	require('./back_arm/f8f8f8.png'),
 ]
+
 
 const back_banding = [
 	require('./back_banding/00b7ee.png'),
+	require('./back_banding/001c58.png'),
+	require('./back_banding/0075a9.png'),
+	require('./back_banding/80c269.png'),
+	require('./back_banding/097c25.png'),
+	require('./back_banding/580b0b.png'),
+	require('./back_banding/601986.png'),
+	require('./back_banding/626262.png'),
+	require('./back_banding/910000.png'),
+	require('./back_banding/aa89bd.png'),
+	require('./back_banding/back.png'),
+	require('./back_banding/cfcfcf.png'),
+	require('./back_banding/f29c9f.png'),
+	require('./back_banding/f39800.png'),
+	require('./back_banding/fff45c.png'),
 ]
 
 const back_body = [
-	require('./back_body/00v7ee.png'),
+	require('./back_body/00b7ee.png'),
+	require('./back_body/001c58.png'),
+	require('./back_body/0075a9.png'),
+	require('./back_body/80c269.png'),
+	require('./back_body/097c25.png'),
+	require('./back_body/580b0b.png'),
+	require('./back_body/601986.png'),
+	require('./back_body/626262.png'),
+	require('./back_body/910000.png'),
+	require('./back_body/aa89bd.png'),
+	require('./back_body/back.png'),
+	require('./back_body/cfcfcf.png'),
+	require('./back_body/f29c9f.png'),
+	require('./back_body/f39800.png'),
+	require('./back_body/fff45c.png'),
 ]
 
 const back_stripe = [
 	require('./back_stripe/00b7ee.png'),
+	require('./back_stripe/001c58.png'),
+	require('./back_stripe/0075a9.png'),
+	require('./back_stripe/80c269.png'),
+	require('./back_stripe/097c25.png'),
+	require('./back_stripe/580b0b.png'),
+	require('./back_stripe/232323.png'),
+	require('./back_stripe/601986.png'),
+	require('./back_stripe/910000.png'),
+	require('./back_stripe/aa89bd.png'),
+	require('./back_stripe/cfcfcf.png'),
+	require('./back_stripe/cfcfcf.png'),
+	require('./back_stripe/f29c9f.png'),
+	require('./back_stripe/f39800.png'),
+	require('./back_stripe/fff45c.png'),
+	require('./back_stripe/ffffff.png'),
+
 ]
 
 const front_arm = [
 	require('./front_arm/2a2a2a_arm.png'),
+	require('./front_arm/2e4373_arm.png'),
+	require('./front_arm/4a321f_arm.png'),
+	require('./front_arm/4f4f53_arm.png'),
+	require('./front_arm/485a2f_arm.png'),
+	require('./front_arm/316863_arm.png'),
+	require('./front_arm/462355_arm.png'),
+	require('./front_arm/a65824_arm.png'),
+	require('./front_arm/f8f8f8_arm.png'),
+	require('./front_arm/942727_arm.png'),
 ]
 
 const front_body = [
+	require('./front_body/00b7ee_body.png'),
+	require('./front_body/001c58_body.png'),
+	require('./front_body/0075a9_body.png'),
+	require('./front_body/80c269_body.png'),
+	require('./front_body/097c25_body.png'),
+	require('./front_body/580b0b_body.png'),
+	require('./front_body/601986_body.png'),
+	require('./front_body/626262_body.png'),
+	require('./front_body/910000_body.png'),
+	require('./front_body/aa89bd_body.png'),
+	require('./front_body/front_body.png'),
+	require('./front_body/cfcfcf_body.png'),
 	require('./front_body/f29c9f_body.png'),
+	require('./front_body/f39800_body.png'),
+	require('./front_body/fff45c_body.png'),
 ]
 
 const front_button = [
 	require('./front_button/00b7ee_button.png'),
+	require('./front_button/001c58_button.png'),
+	require('./front_button/0075a9_button.png'),
+	require('./front_button/80c269_button.png'),
+	require('./front_button/097c25_button.png'),
+	require('./front_button/580b0b_button.png'),
+	require('./front_button/601986_button.png'),
+	require('./front_button/626262_button.png'),
+	require('./front_button/910000_button.png'),
+	require('./front_button/aa89bd_button.png'),
+	require('./front_button/front_button.png'),
+	require('./front_button/cfcfcf_button.png'),
+	require('./front_button/f29c9f_button.png'),
+	require('./front_button/f39800_button.png'),
+	require('./front_button/fff45c_button.png'),
 ]
 
 const front_stripe = [
 	require('./front_stripe/00b7ee_stripe.png'),
+	require('./front_stripe/001c58_stripe.png'),
+	require('./front_stripe/0075a9_stripe.png'),
+	require('./front_stripe/80c269_stripe.png'),
+	require('./front_stripe/097c25_stripe.png'),
+	require('./front_stripe/580b0b_stripe.png'),
+	require('./front_stripe/232323_stripe.png'),
+	require('./front_stripe/601986_stripe.png'),
+	require('./front_stripe/910000_stripe.png'),
+	require('./front_stripe/aa89bd_stripe.png'),
+	require('./front_stripe/cfcfcf_stripe.png'),
+	require('./front_stripe/cfcfcf_stripe.png'),
+	require('./front_stripe/f29c9f_stripe.png'),
+	require('./front_stripe/f39800_stripe.png'),
+	require('./front_stripe/fff45c_stripe.png'),
+	require('./front_stripe/ffffff_stripe.png'),
 ]
 export {
 	hairlist, 
