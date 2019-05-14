17ae222c4b83c43cfe0777adf6d686cfa668f1d6
sally20921 <sally20921@snu.ac.kr>
2019-05-14 19:24:10 +0900

    uploading files

--- frontend/src/components/DesignPage/FabricCanvas.js
+++ frontend/src/components/DesignPage/FabricCanvas.js
@@ -5,39 +5,53 @@ import ImageUploader from 'react-images-upload';
 
 class FabricCanvas extends React.Component{
     
-    /*constructor(props) {
+    constructor(props) {
         super(props);
         this.state = {pictures : []};
         this.onDrop = this.onDrop.bind(this);
     }
 
-    onDrop(picture) {
-        console.log(picture);
-       this.setState({
-            pictures: this.state.pictures.concat(picture),
-        });
-       
-      let imageUrl = './img/tshirt1_back.jpg';
-      var img = new Image();
-      img.src = imageUrl;
-    File -> 
-      javascript Image 형식 -> 
-      var Img = new fabric.Image(img);
-      this.the_canvas.add(Img);
-      this.the_canvas.renderAll();
+    onDrop(e) {
+        e.preventDefault();
+        var preview = document.getElementById('img');
+        var file = document.getElementById('input').files[0];
+
+        let reader = new FileReader();
+        reader.addEventListener("load", function() {
+            preview.src = reader.result;
+        },false);
+
+        /*reader.onloadend = () => {
+            var img = new Image(40,40);
+	        img.src = reader.result;
+            
+            var imgInstance = new fabric.Image(img, {
+                width: 899,
+                height: 959,
+                the_type: "upload",
+                zIndex: 2
+            });    
+
+            this.the_canvas.add(imgInstance);
+        }*/
+        
+        if (file) {
+            reader.readAsDataURL(file);
+        }
+
      
-    }*/
+    }
    
     componentDidMount(){
 
-        this.the_canvas = new fabric.StaticCanvas('main-canvas', {
+        this.the_canvas = new fabric.Canvas('main-canvas', {
             preserveObjectStacking: true,
             height:959,
             width:899,
         });
     }
 
-    componentWillReceiveProps = (newprops) =>{
+    componentWillReceiveProps = (newprops) => {
 
         // If Updated Item is not the same as the old one
         //         => Update the canvas with newer item
@@ -94,14 +108,20 @@ class FabricCanvas extends React.Component{
                 >
                 </canvas>
 
-               
-                  <ImageUploader 
+                  <input type = "file"
+                         id = "input" 
+                         onchange = {this.onDrop} />
+                  <img src = "" 
+                        height = "40"
+                        width = "40"
+                        id = "img" />
+                  {/*<ImageUploader 
                     withIcon = {true}
                     buttonText = 'Choose images'
                     onChange = {this.onDrop}
                     imgExtension = {['.jpg', '.gif', 'png', '.gif']}
                     withPreview = {true}
-                  />
+                  />*/}
             </div>
         );
     }
