e42374ae2812e150b160c235c427ec829c194d9a
SeungaShin <tmddk2709@naver.com>
2019-05-23 23:14:03 +0900

    Admin user is not on the delete list now

--- frontend/src/components/GroupPage/GroupAdminPage.js
+++ frontend/src/components/GroupPage/GroupAdminPage.js
@@ -15,6 +15,11 @@ class GroupAdminPage extends React.Component {
 			)
 		}
 
+		let admin_userlist = this.props.group_users.filter(user => {
+			console.log("username: ", user.username)
+			return (user.username !== this.props.user.split(":")[0])
+		})
+
 		return (
 			<div className="GroupAdminPage">
 				<NavBar />
@@ -22,45 +27,42 @@ class GroupAdminPage extends React.Component {
 				<section className="wrap clear col3">
 					<div className="aside">
 						<h2 className="h_white">GROUP INFO</h2>
-							<div className="content">
-							<ChangeGroupInfo
-								group={this.props.now_group[0]}
-								onClickChangeSubmit={this.props.onChangeGroupInfo}
-							/>
-							</div>
+						<div className="content">
+						<ChangeGroupInfo
+							group={this.props.now_group[0]}
+							onClickChangeSubmit={this.props.onChangeGroupInfo}
+						/>
+						</div>
 					</div>
 					<div className="main">
 						<h2 className="h_white">DESIGNS</h2>
-							<div className="content">
-							<GroupDesignList
-								groupid={this.props.now_group[0].id}
-								designlist={this.props.group_designs}
-								onClickDeleteDesign={this.props.onDeleteDesign}
-							/>
+						<div className="content">
+						<GroupDesignList
+							groupid={this.props.now_group[0].id}
+							designlist={this.props.group_designs}
+							onClickDeleteDesign={this.props.onDeleteDesign}
+						/>
 
-							</div>
+						</div>
 					</div>
 					<div className="aside">
 						<h2 className="h_black">MEMBER LIST</h2>
-							<div className="content">
-							<GroupUserList
-								groupid={this.props.now_group[0].id}
-								userlist={this.props.group_users}
-								onClickDeleteUser={this.props.onDeleteUser}
-							/>
-							</div>
+						<div className="content">
+						<GroupUserList
+							groupid={this.props.now_group[0].id}
+							userlist={admin_userlist}
+							onClickDeleteUser={this.props.onDeleteUser}
+						/>
+						</div>
 					</div>
 				</section>
 				</div>
-
-
-
-		
 		)
 	}
 }
 
 const mapStateToProps = (state) => ({
+	user: state.authorization,
 	now_group: state.now_group,
     group_users: state.group_users,
     group_designs: state.group_designs,
