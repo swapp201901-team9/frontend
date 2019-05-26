4432a32d99b7fd8214b49377df361a65ba8972e0
sally20921 <sally20921@snu.ac.kr>
2019-05-26 14:06:15 +0900

    added a bunch in package.json very uneasy about this

--- frontend/src/components/DesignPage/DesignPage.test.js
+++ frontend/src/components/DesignPage/DesignPage.test.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import {fabric} from 'fabric';
 //import DesignPage from './DesignPage';
-import {create} from 'react-test-renderer'
+//import FabricCanvas from './FabricCanvas';
+import TemplateList from './TemplateList';
+import TemplateListItem from './TemplateListItem';
+//import {create} from 'react-test-renderer';
+import {mount, shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
+
+//Enzyme.configure({ adapter: new Adapter() });
 
 describe('My first snapshot test', ()=> {
     test('testing design page', () => {
@@ -8,3 +17,62 @@ describe('My first snapshot test', ()=> {
         //expect(tree.toJSON()).toMatchSnapshot();
     })
 })
+
+describe('FabricCanvas', ()=> {
+    it ('renders without crashing', ()=> {
+        //const component = create(<FabricCanvas/>);
+       //const instance = component.getInstance();
+        //await instance.componentDidMount();
+
+         //const div = document.createElement('div');
+         //ReactDOM.render(<FabricCanvas/>, div);
+         //ReactDOM.unmountComponentAtNode(div);
+
+         //const wrapper = shallow(<FabricCanvas/>);
+         //expect(wrapper.length).toBe(1);
+    })
+})
+
+describe('DesignPage', ()=> {
+    it('renders without crashing', ()=>{
+        //const component = create(<DesignPage/>);
+        //const instance = component.getInstance();
+        //await instance.componentDidMount();
+
+        //const div = document.createElement('div');
+        //ReactDOM.render(<Clock/>, div);
+
+        //mount(<DesignPage/>);
+        //shallow(<DesignPage />);
+    })
+})
+
+describe('TemplateList', ()=> {
+    it('renders without crashing', ()=>{
+        //const component = create(<DesignPage/>);
+        //const instance = component.getInstance();
+        //await instance.componentDidMount();
+
+        //const div = document.createElement('div');
+        //ReactDOM.render(<Clock/>, div);
+
+        //mount(<DesignPage/>);
+        //shallow(<DesignPage />);
+    })
+})
+
+describe('TemplateListItem', ()=> {
+    it('renders without crashing', ()=>{
+        //const component = create(<DesignPage/>);
+        //const instance = component.getInstance();
+        //await instance.componentDidMount();
+
+        //const div = document.createElement('div');
+        //ReactDOM.render(<Clock/>, div);
+
+        //mount(<DesignPage/>);
+        //shallow(<DesignPage />);
+    })
+})
+
+
