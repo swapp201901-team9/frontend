4207e975eecd1edeae68b9c48003e4131c4efafc
sally20921 <sally20921@snu.ac.kr>
2019-06-05 17:43:34 +0900

    sleeve fcfcfc

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -14,10 +14,10 @@ const homepageInitialState = {
     now_group: null, //현재 작업 or 구경(?) 중인 그룹
     now_design: {
         detail_body: "001c58",
-        detail_sleeve: "f8f8f8",
+        detail_sleeve: "fcfcfc",
         detail_banding: "001c58",
         detail_stripes: "fcfcfc",
-        detail_buttons: "001c58"
+        detail_buttons: "fcfcfc"
     }, //현재 메인 페이지에서 작업 중인(화면에 보이는) 디자인
 
     my_requests: [],
