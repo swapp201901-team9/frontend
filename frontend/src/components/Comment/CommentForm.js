feacae7a1966bc43aea7640c788630eda63b343c
SeungaShin <tmddk2709@naver.com>
2019-06-16 17:39:54 +0900

    Working on comment

---
+++ frontend/src/components/Comment/CommentForm.js
@@ -0,0 +1,76 @@
+import React from "react";
+import {connect} from 'react-redux';
+import { toAddComment } from "../../actions";
+
+class CommentForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.contents = "";
+    // bind context to methods
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  /**
+   * Form submit handler
+   */
+  onSubmit(e) {
+    // prevent default form submission
+    e.preventDefault();
+
+    if (!this.isFormValid()) {
+      this.setState({ error: "All fields are required." });
+      return;
+    }
+
+    this.props.onAddComment(this.props.designid, this.contents)
+  }
+
+  /**
+   * Simple validation
+   */
+  isFormValid() {
+    return this.contents !== "";
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.onSubmit}>
+          {/* <div className="form-group">
+            <input
+              onChange={this.handleFieldChange}
+              value={this.state.comment.name}
+              className="form-control"
+              placeholder="😎 Your Name"
+              name="name"
+              type="text"
+            />
+          </div> */}
+
+          <div className="form-group">
+            <textarea
+              value={this.contents}
+              className="form-control"
+              placeholder="🤬 Your Comment"
+              name="message"
+              rows="5"
+            />
+          </div>
+
+          <div className="form-group">
+            <button className="btn btn-primary">
+              Comment &#10148;
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  onAddComment: (designid, contents) => dispatch(toAddComment(designid, contents)),
+})
+
+export default connect (null, mapDispatchToProps)(CommentForm)
