f00a0f02ac7b1013ef4b87c2028eb30ec893f582
SeungaShin <tmddk2709@naver.com>
2019-06-18 02:28:21 +0900

    없는 아이디/틀린 비밀번호로 로그인 시도 시의 에러 메시지 바꾸기

--- frontend/src/store/homepage/new_sagas.js
+++ frontend/src/store/homepage/new_sagas.js
@@ -854,7 +854,7 @@ function *signIn(data) {
         yield put(actions.changeUrl('/main/'));
     }
     catch(error) {
-        alert("아이디 또는 비밀번호가 ");
+        alert("아이디 또는 비밀번호를 다시 확인해주세요.");
         console.log(error)
     }
 }
